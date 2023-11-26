ca79c085d9ad80663aacfde16cc0f15ee7dc1b35
tea bamboo <teabamboongu@gmail.com>
2023-11-26 21:40:42 +0800

    code

---
+++ script/TangNFT.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/TangNFT.sol";
+
+contract TangNFTScript is Script {
+
+    function setUp() public {
+        
+    }
+
+    function run() public {
+        vm.startBroadcast();
+        TangNFT tangNFT = new TangNFT("TangNFT","TNFT");
+        vm.stopBroadcast();
+    }
+}
