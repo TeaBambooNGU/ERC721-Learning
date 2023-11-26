0eaa361fa50d41ce46fd8cbaf077ec644d49bba4
tea bamboo <teabamboongu@gmail.com>
2023-11-26 19:12:34 +0800

    NFT合约测试代码

---
+++ src/TangNFT.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract TangNFT is ERC721, Ownable {
+    // 最大NFT发行量
+    uint256 public constant MAX_TANGS = 10000;
+    uint256 public total_mint = 0;
+    using Strings for uint;
+    constructor(string memory _name, string memory _symbol) 
+        ERC721(_name, _symbol) Ownable(msg.sender) {
+        
+    }
+    // 暂时用无聊猿的ipfs 
+    function _baseURI() internal override pure returns (string memory) {
+        return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+    }
+    
+    function _safeMint(address to, uint256 tokenId, bytes memory data) internal override  {
+        require(total_mint < MAX_TANGS, "out of max mint");
+        total_mint +=1;
+        _mint(to, tokenId);
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to,tokenId,"");
+    }
+
+    function tokenURI(uint tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString()) : "";
+    }
+}
---
+++ test/TangNFT.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "forge-std/test.sol";
+import "../src/TangNFT.sol";
+
+contract TangNFTtest is Test {
+    TangNFT private tangNFT ;
+    
+    function setUp() public {
+        tangNFT = new TangNFT("TangNFT","TNFT");
+    }
+
+    function testSafeMint() public {
+        address getNFTWallet = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+        uint tokenId = 19970710;
+        tangNFT.safeMint(getNFTWallet,tokenId);
+        console2.logUint(tangNFT.balanceOf(getNFTWallet));
+        console2.logString(tangNFT.tokenURI(tokenId));
+        assert  (1 == tangNFT.balanceOf(getNFTWallet));
+    }
+
+    function testTokenURI() public {
+        string memory tokenURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/19970710";
+        uint tokenId = 19970710;
+        address getNFTWallet = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+        tangNFT.safeMint(getNFTWallet,tokenId);
+        string memory result = tangNFT.tokenURI(tokenId);
+        assert (keccak256(abi.encodePacked(tokenURI)) == keccak256(abi.encodePacked(result)));
+    }
+
+    function testFailTokenURI() public{
+        string memory tokenURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/19970710";
+        uint tokenId = 19970710;
+        string memory result = tangNFT.tokenURI(tokenId);
+        assert (keccak256(abi.encodePacked(tokenURI)) == keccak256(abi.encodePacked(result)));
+    }
+
+}
